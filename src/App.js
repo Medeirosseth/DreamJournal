9332ec0d7339abbaf24e555e70e683851930779b
Seth <Medeirosseth@gamil.com>
2021-11-30 14:28:08 -0800

    update homescreen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { IonApp } from "@ionic/react";
 import Header from "./Components/Header/Header";
 import { Test } from "./Components/Test/Test";
 import Login from "./Login/Login";
+import Home from "./Components/HomeScreen/Home";
 
 function App() {
   return (
   <IonApp >
     <Header />
-
+    <Home />
   </IonApp>
   );
 }
