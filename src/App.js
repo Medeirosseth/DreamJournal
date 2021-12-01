3160f7d7f515fc12b4c4a0bdba630debef11e7e0
Michael Burton <michaelburton@Michaels-MBP.domain>
2021-12-01 15:10:32 -0800

    add components without styling or functionality for feebcak form and settings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { IonApp } from "@ionic/react";
+
 import EntryForm from "./Components/entryForm/EntryForm";
 import Header from "./Components/Header/Header";
 import { Test } from "./Components/Test/Test";
 import Login from "./Login/Login";
+import Settings from "./Components/Settings/Settings";
+import FeedbackForm from "./Components/FeedbackForm/FeedbackForm";
 
 function App() {
   return (
   <IonApp >
-    <Header />
-    <EntryForm/>
+
   </IonApp>
   );
 }
