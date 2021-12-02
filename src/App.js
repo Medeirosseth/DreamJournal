4c1804baab123ca130feee5bb55e05f9c5c16738
Seth <Medeirosseth@gamil.com>
2021-12-02 13:33:19 -0800

    WIP: merge"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { IonApp } from "@ionic/react";
 import Header from "./Components/Header/Header";
 import { Test } from "./Components/Test/Test";
-import Login from "./Login/Login";
 import Register from "./Pages/Register/Register";
 import Home from "./Components/HomeScreen/Home";
 import Entries from "./Entires/Entries";
